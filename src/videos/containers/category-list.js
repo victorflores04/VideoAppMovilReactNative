69162e15f091ef861c3d96acf54cd1b8adeca398
Victor Flores <flores.vicf@gmail.com>
2020-09-15 22:33:44 -0500

    agregando seraradores horizontales

--- src/videos/containers/category-list.js
+++ src/videos/containers/category-list.js
@@ -7,9 +7,9 @@ FlatList,
 } from 'react-native';
 
 import Empty from '../components/emtpy';
-import Separator from '../components/vertical-separator';
+import Separator from '../../sections/components/horizaontal-separator';
 import Suggestions from '../components/suggestions';
-
+import Layout from '../components/category-list-layout';
 
 class CategoriesList extends Component{
     keyExtractor = item => item.id.toString();
@@ -23,6 +23,7 @@ class CategoriesList extends Component{
 
 render(){
     return(
+        <Layout title="Categorias">
         <FlatList
         horizontal={true}
         keyExtractor={this.keyExtractor}
@@ -31,6 +32,8 @@ render(){
         ItemSeparatorComponent={this.itemSeparator}
         renderItem={this.renderItem}
         />
+        </Layout>
+        
     )
 }
 }
