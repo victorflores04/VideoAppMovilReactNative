777bd80a356334c05757b4cbd206b9dddab231a8
Victor Flores <flores.vicf@gmail.com>
2020-09-16 01:14:02 -0500

    agregando buffer del video

--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -3,18 +3,42 @@ import Layout from '../components/layout';
 import Video from 'react-native-video';
 import {
 StyleSheet,
+ActivityIndicator,
 }from 'react-native';
 
 
 class Player extends Component{
+    state={
+        loading: true,
+    }
+    onBuffer = ({isBuffering})=>{
+        this.setState({
+            loading: isBuffering
+        })
+    }
+    onLoad=()=>{
+        this.setState({
+            loading: false
+        })
+    }
 render(){
     return(
         <Layout
+        loading={this.setState.loading}
         video={
             <Video 
             source={{uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"}}
             style={styles.video}
-            resizeMode="contain"/>
+            resizeMode="contain"
+            onBuffer={this.onBuffer}
+            onLoad={this.onLoad}
+            />
+           
+        }
+        loader={
+            <ActivityIndicator 
+            color="red"
+            />
         }
         />
     )
