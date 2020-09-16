325f061122a8ef606b62749570a4163f045ed0b2
Victor Flores <flores.vicf@gmail.com>
2020-09-16 00:54:38 -0500

    creacion del reproductor de vide

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestions-list';
 import API from './utils/api';
 import CategoryList from './src/videos/containers/category-list';
-import Video from 'react-native-video';
+import Player from './src/player/containers/player';
 
 export default class App extends Component<Props>{
   state={
@@ -27,22 +27,8 @@ export default class App extends Component<Props>{
   render(){
     return(
       <Home>
-        <Header><Text>Icon and User</Text></Header>
-        <View style={{
-          flex:1,
-          height:100,
-          
-        }}>
-        <Video source={{uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"}}
-        style={{
-          position:'absolute',
-          left:0,
-          right:0,
-          bottom:0,
-          top:0,
-        }}
-        resizeMode="cover"/>
-        </View>
+        <Header/>
+        <Player/>
         <Text>Buscador</Text>
         <CategoryList
          list={this.state.categoryList}
---
+++ src/player/containers/player.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Layout from '../components/layout';
+import Video from 'react-native-video';
+import {
+StyleSheet,
+}from 'react-native';
+
+
+class Player extends Component{
+render(){
+    return(
+        <Layout
+        video={
+            <Video 
+            source={{uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"}}
+            style={styles.video}
+            resizeMode="contain"/>
+        }
+        />
+    )
+}
+}
+const styles =StyleSheet.create({
+    video:{
+        position: 'absolute',
+        left:0,
+        right:0,
+        bottom:0,
+        top:0,
+    }
+});
+export default Player;
