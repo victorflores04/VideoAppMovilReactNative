b5dba8d028b2122473ea7f790434179efa0630b7
Victor Flores <flores.vicf@gmail.com>
2020-09-15 20:40:24 -0500

    Separadores en listas vacias y reutilizables

---
+++ src/videos/components/emtpy.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+View,
+Text,
+StyleSheet,
+} from 'react-native';
+
+function Emtpy(props){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>{props.text}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 20,
+
+    },
+    text:{
+        fontSize:16,
+
+    }
+});
+export default Emtpy;
--- src/videos/containers/suggestions-list.js
+++ src/videos/containers/suggestions-list.js
@@ -6,17 +6,22 @@ import {
 } from 'react-native';
 
 import Layout from '../components/suggestions-list-layout';
+import Empty from '../components/emtpy';
+import Separator from '../components/vertical-separator';
 
 class SuggestionList extends Component{
+
+    renderEmtpy =()=> <Empty text="No hay sugerencias :c"/>
+    itemSeparator =()=> <Separator />
     render(){
         const list =[
             {
-                title: 'ing',
+                title: 'Avengers',
                 key:'1',
 
             },
             {
-                title: 'mari',
+                title: 'Pokemon',
                 key:'2',
             },
         ]
@@ -24,6 +29,8 @@ class SuggestionList extends Component{
             <Layout title="Sugerencias para ti">
                 <FlatList
                     data={list} 
+                    ListEmptyComponent={this.renderEmtpy}
+                    ItemSeparatorComponent={this.itemSeparator}
                     renderItem={({item})=><Text>{item.title}</Text>}
                     />
             </Layout>
